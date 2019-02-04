cedc93cfa3803e8da5caa8389e95b068e46a200f
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-04 02:31:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,10 @@ class Student
   
   def self.create_table 
     new_student = self.new 
-    new_student.id = 
+    new_student.id = row[0]
+    new_student.name = row [1]
+    new_student.grade = row[2]
+    new_student
     
   end 
   
