43248f3db3ea7e6aa9e59f9483497240ac8402ac
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-04 03:29:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,8 +66,8 @@ class Student
         LIMIT 1
     SQL
     
-      DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
+    DB[:conn].execute(sql,name).map do |row|
+        self.new_from_db(row)
       end.first
   end 
   
