14b04ec4cd6b611b52545600f6c3747d83cb6501
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-04 02:54:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,7 @@ class Student
   end 
   
   def self.update
+    sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
+      DB[:conn].execute(sql, self.name, self.album, self.id)
   end
-
 end
