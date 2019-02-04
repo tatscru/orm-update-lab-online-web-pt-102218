340c8c3c8460f7034c2d7c84d597cb8f2ad624cc
tatiana.mojica <tatiana.mojica@bustle.com>
2019-02-04 02:36:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,8 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-     CREATE TABLE IF NOT EXISTS 
+     CREATE TABLE IF NOT EXISTS students(
+     id PRIMARY KEY INTEGER, )
     
   end 
   
